c92a5124a26dedc5f6ce6629cb420c519bcbf06f
wfzhang <wfzhang@mobvoi.com>
2017-07-08 14:30:25 +0800

    yii2 admin

--- vendor/yiisoft/extensions.php
+++ vendor/yiisoft/extensions.php
@@ -76,4 +76,13 @@ return array (
     ),
     'bootstrap' => 'dektrium\\user\\Bootstrap',
   ),
+  'mdmsoft/yii2-admin' => 
+  array (
+    'name' => 'mdmsoft/yii2-admin',
+    'version' => '2.7.0.0',
+    'alias' => 
+    array (
+      '@mdm/admin' => $vendorDir . '/mdmsoft/yii2-admin',
+    ),
+  ),
 );
