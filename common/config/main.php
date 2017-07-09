402b7dc879c8d15618973eb3da87e8633f13ba46
wfzhang <wfzhang@mobvoi.com>
2017-07-09 17:38:46 +0800

    yii翻译文件

--- common/config/main.php
+++ common/config/main.php
@@ -1,9 +1,19 @@
 <?php
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'timeZone'=>'Asia/Chongqing',
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
-        ]
+        ],
+        // app 是 t函数的参数
+        'i18n' => [
+            'translations' => [
+                'app' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@common/messages',
+                ],
+            ],
+        ],
     ],
 ];
