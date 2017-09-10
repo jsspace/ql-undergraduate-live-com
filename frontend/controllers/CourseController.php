65da15d3c4e96cb2fd4668ee5914f139df06a14f
bjfudxj <1031848267@qq.com>
2017-09-10 11:07:09 +0800

    update

--- frontend/controllers/CourseController.php
+++ frontend/controllers/CourseController.php
@@ -3,7 +3,7 @@
 namespace frontend\controllers;
 
 use yii\web\Controller;
-
+use backend\models\CourseCategory;
 
 class CourseController extends Controller
 {
@@ -26,6 +26,25 @@ class CourseController extends Controller
     
     public function actionList()
     {
+        $catModels = CourseCategory::find()
+        ->all();
+
+        $firArr = array();
+
+        foreach ($catModels as $catModelKey => $catModel) {
+            if ($catModel->parent_id == 0) {
+                $firArr[$catModelKey] = array();
+                $firArr[$catModelKey]['firModel'] = $catModel;
+                $firArr[$catModelKey]['child'] = array();
+                foreach ($catModels as $subModelKey => $subModel) {
+                    if ($subModel->parent_id == $catModel->id) {
+                        $firArr[$catModelKey]['child'][$subModelKey] = $subModel;
+                    }
+                }
+            }
+        }
+        print_r(json_encode($firArr));
+        die();
         return $this->render('list');
     }
     
