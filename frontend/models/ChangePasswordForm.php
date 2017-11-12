a810dd7be49f5e1c5e69f684f1c1bf39e36d60cb
= <=>
2017-11-12 22:02:36 +0800

    修改密码

--- frontend/models/ChangePasswordForm.php
+++ frontend/models/ChangePasswordForm.php
@@ -1,6 +1,7 @@
 <?php
 namespace frontend\models;
 
+use Yii;
 use yii\base\Model;
 use common\models\User;
 
@@ -14,6 +15,11 @@ class ChangePasswordForm extends Model
     public $password;
     
     /**
+     * @var \common\models\User
+     */
+    private $_user;
+    
+    /**
      * @inheritdoc
      */
     public function rules()
@@ -22,27 +28,28 @@ class ChangePasswordForm extends Model
             ['phone', 'trim'],
             ['phone', 'required'],
             ['phone','match','pattern'=>'/^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/','message'=>'{attribute}号码格式错误，必须为1开头的11位纯数字'],
-            ['phone', 'string', 'min'=>11,'max' => 11,'on' => ['default','login_sms_code']],
-            [['phone'], 'isPhoneExist', 'skipOnEmpty' => false, 'skipOnError' => false],
+            ['phone', 'string', 'min'=>11,'max' => 11],
+            ['phone', 'is_phone_exist', 'skipOnEmpty' => false, 'skipOnError' => false],
             
+            ['change_password_code', 'string', 'min'=>6,'max' => 6],
             ['change_password_code', 'required','on' => ['default','login_sms_code']],
             ['change_password_code', 'integer','on' => ['default','login_sms_code']],
-            [['change_password_code'], 'getLoginCode', 'skipOnEmpty' => false, 'skipOnError' => false],
+            [['change_password_code'], 'get_change_password_code', 'skipOnEmpty' => false, 'skipOnError' => false],
             
             ['password', 'required'],
             ['password', 'string', 'min' => 6],
         ];
     }
     
-    public function isPhoneExist($attribute, $params)
+    public function is_phone_exist($attribute, $params)
     {
-        $user = User::findByPhone($this->phone);
-        if (!$user) {
+        $this->_user = User::findByPhone($this->phone);
+        if (!$this->_user) {
             $this->addError('phone', '不存在这个手机号用户！');
         }
     }
     
-    public function getLoginCode($attribute, $params)
+    public function get_change_password_code($attribute, $params)
     {
         //检查session是否打开
         if(!Yii::$app->session->isActive){
@@ -54,7 +61,7 @@ class ChangePasswordForm extends Model
             $change_password_code = $session['change_password_code']['code'];
             $expire_time = $session['change_password_code']['expire_time'];
             if (time()-$expire_time < 0) {
-                if ($this->smscode != $session['change_password_code']['code']) {
+                if ($this->change_password_code != $session['change_password_code']['code']) {
                     $this->addError('change_password_code', '验证码的值输入错误！');
                 }
             } else {
