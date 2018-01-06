918e55161f3e6aaffb05c4df48cff324d5aafd3a
ypgao <ypgao@mobvoi.com>
2018-01-06 18:07:16 +0800

    入学须知

--- frontend/views/member/index.php
+++ frontend/views/member/index.php
@@ -15,6 +15,15 @@ $this->title = '购买vip会员';
 ?>
 <div class="site-vip">
     <div class="inner">
+    <div class="vip-section vip-tip">
+        <p class="vip-title"><i class="icon ion-ios-navigate-outline"></i>入学须知</p>
+        <div class="tip-con">
+            <p>感谢您成为本校的学员，在办理学习会员前， 请认真阅读以下须知：</p>
+            <p>1.请持《入学通知书》于2017年8月22日当天到我校报到注册并办理入学手续。</p>
+            <p>2.新生正式报到时，学校将统一赠送一套卧具（被套、棉被、草席、枕芯、蚊帐、床单、枕套）和三年学生平安保险。</p>
+            <p>3.新生来校报到时还应携带本人及监护人户口本及身份证复印件一式两份。</p>
+        </div>
+    </div>
     <?php 
         $str = '';
         foreach($member_items as $item) {
