d37729f328e08703ad0a63f86fe31f6fafa74ba6
ypgao <ypgao@mobvoi.com>
2018-01-27 17:38:31 +0800

    message add

--- frontend/views/user/message.php
+++ frontend/views/user/message.php
@@ -20,6 +20,47 @@ $this->title = '个人中心';
 <div class="user-wrapper">
     <?= $this->render('lmenu') ?>
     <div class="right-content">
-       
+       <p class="user-right-title">消息通知</p>
+       <div class="status-select-wrapper _order-status">
+            <p class="current-status">全部状态</p>
+            <ul class="status-list">
+                <li class="active">全部状态</li>
+                <li>已读</li>
+                <li>未读</li>
+            </ul>
+        </div>
+
+        <ul class="message-list">
+            <li>
+                <dl>
+                    <dt>
+                        <span class="msg-date">2017-12-31 11:22</span>
+                        <a href="/user/message/view?id=2299">致所有邢帅教育学员的一封信</a>
+                        <span class="unread">未读</span>
+                    </dt>
+                    <dd>
+                        <span class="msg-con">
+                        亲爱的学员们：曾经，书信在车马邮件都慢的时代里，哪怕寥寥数语，也是纸短情长。很高...
+                        </span>
+                        <a class="msg-more" href="/user/message/view?id=2299">查看详细 &gt;</a>
+                    </dd>
+                </dl>
+            </li>
+            <li>
+                <dl>
+                    <dt>
+                        <span class="msg-date">2017-12-31 11:22</span>
+                        <a href="/user/message/view?id=2299">致所有邢帅教育学员的一封信</a>
+                        <span class="unread">未读</span>
+                    </dt>
+                    <dd>
+                        <span class="msg-con">
+                        亲爱的学员们：曾经，书信在车马邮件都慢的时代里，哪怕寥寥数语，也是纸短情长。很高...
+                        </span>
+                        <a class="msg-more" href="/user/message/view?id=2299">查看详细 &gt;</a>
+                    </dd>
+                </dl>
+            </li>
+        </ul>
     </div>
 </div>
