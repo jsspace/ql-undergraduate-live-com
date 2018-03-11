0fe1ad5dfcd46f48ebc3665c7bcbfb961d36da9a
ypgao <ypgao@mobvoi.com>
2018-03-11 16:16:13 +0800

    添加修改密码页面

--- frontend/views/user/change-password.php
+++ frontend/views/user/change-password.php
@@ -23,13 +23,15 @@ $this->title = '个人中心';
     <div class="right-content">
         <div class="pass-reset">
            <div class="site-reset-password">
-                <p>Please choose your new password:</p>
+                <p>修改密码:</p>
                 <div class="row">
                     <div class="col-lg-5">
                         <?php $form = ActiveForm::begin(['id' => 'reset-password-form']); ?>
-
+                            <input type="password" placeholder="现有密码" id="historyPwd" />
+                            <input type="password" placeholder="新密码" id="newPwd"/>
+                            <input type="password" placeholder="重新输入新密码" id="repeatPwd"/>
                             <div class="form-group">
-                                <?= Html::submitButton('Save', ['class' => 'btn btn-primary']) ?>
+                                <?= Html::submitButton('保存', ['class' => 'btn btn-primary']) ?>
                             </div>
 
                         <?php ActiveForm::end(); ?>
@@ -39,3 +41,50 @@ $this->title = '个人中心';
         </div>
     </div>
 </div>
+
+<script>
+    $('button[type="submit"]').on('click', function () {
+        var historyPwd = $('#historyPwd').val();
+        var newPwd = $('#newPwd').val();
+        var repeatPwd = $('#repeatPwd').val();
+        if (!historyPwd) {
+            alert('请输入密码');
+            $('#historyPwd').focus();
+            return false;
+        }
+        if (!newPwd) {
+            alert('请输入新密码');
+            $('#newPwd').focus();
+            return false;
+        }
+        if (!repeatPwd) {
+            alert('请重新输入新密码');
+            $('#repeatPwd').focus();
+            return false;
+        }
+        if (newPwd !== repeatPwd) {
+            alert('两次密码输入不同，请重新输入');
+            $('#repeatPwd').focus();
+            return false;
+        }
+        $.ajax({
+            url: '/site/chang-password-code',
+            type: 'post',
+            dataType:"json",
+            async: false,
+            data: {
+                '_csrf-frontend': $('meta[name=csrf-token]').attr('content'),
+                'old_password': historyPwd,
+                'new_password': newPwd,
+                'renew_password': repeatPwd
+            },
+            success: function (data) {
+                if (data.code !== 0) {
+                    alert(data.message);
+                } else {
+                    window.location.href = '/user/info';
+                }
+            }
+        });
+    });
+</script>
