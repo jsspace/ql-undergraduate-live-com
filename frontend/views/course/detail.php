098df0788e1ab2aa9465eaca693785c6ddf503af
wfzhang <wfzhang@mobvoi.com>
2018-03-11 13:38:30 +0800

    update share

--- frontend/views/course/detail.php
+++ frontend/views/course/detail.php
@@ -14,7 +14,7 @@ $userid = Yii::$app->user->id;
 $course = $courseDetail['course'];
 $cousechild = $courseDetail['coursechild'];
 $share_title = '精品课程，超低优惠，快来学习吧！';
-$share_url = Url::to(['course/detail', 'courseid'=>$course->id, 'invite'=>$userid], true);
+$share_url = 'http://www.kaoben.top/course/detail?courseid='.$course->id.'&invite='.$userid;
 $news = array(
     "PicUrl" =>'http://www.kaoben.top/img/share-logo.png',
     "Description"=>"活到老，学到老，快来和大家一起学习吧！",
