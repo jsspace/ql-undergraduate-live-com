4094b741ef4b622b65ae79a274d01a44589575fe
= <=>
2018-03-18 10:54:11 +0800

    login insert session

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -76,6 +76,18 @@ class SiteController extends Controller
 
         $model = new LoginForm();
         if ($model->load(Yii::$app->request->post()) && $model->login()) {
+            //使用session和表tbl_admin_session记录登录账号的token:time&id&ip,并进行MD5加密
+            $id = Yii::$app->user->id;     //登录用户的ID
+            
+            $username = Yii::$app->user->identity->username; //登录账号
+            $ip = Yii::$app->request->userIP; //登录用户主机IP
+            $token = md5(sprintf("%s&%s&%s",time(),$id,$ip));  //将用户登录时的时间、用户ID和IP联合加密成token存入表
+            
+            $session = Yii::$app->session;
+            $session->set(md5(sprintf("%s&%s",$id,$username)),$token);  //将token存到session变量中
+            //存session token值没必要取键名为$id&$username ,目的是标识用户登录token的键，$id或$username就可以
+            
+            $model->insertSession($id,$token);//将token存到tbl_admin_session
             return $this->goBack();
         } else {
             return $this->render('login', [
