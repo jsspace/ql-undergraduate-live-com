33352c2a70c199363b071155def800c856ad626e
wfzhang <wfzhang@mobvoi.com>
2018-05-20 11:54:14 +0800

    更换项目为fanghua

--- environments/prod/common/config/params-local.php
+++ environments/prod/common/config/params-local.php
@@ -1,7 +1,2 @@
 <?php
-return [
-    'access_key' => 'BpA5RUTf1eWdiDpsRrosEJ-i9CroZjj9Gi4NOw5t',
-    'secret_key' => 'errjOOqxbwghY96t1a4bSP-ERR-42bHqEI_4H-15',
-    'public_bucket' => 'youshilian',
-    'get_source_host' => 'http://oyxhwhkdy.bkt.clouddn.com'
-];
+return [];
