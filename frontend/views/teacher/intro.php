df619b17c9d8a84dd77f54867c6364b8c8663c81
minyi <minyi@mobvoi.com>
2018-06-30 14:35:23 +0800

    teach-page

--- frontend/views/teacher/intro.php
+++ frontend/views/teacher/intro.php
@@ -21,94 +21,85 @@ $this->title = '教研队伍';
         <ul id="big_list">
         <li>
             <span>
-                <h4><code>陈数</code> 教授<p>山东师范大学</p></h4>
-                <p>1982年于南京工学院（现东南大学）无线电工程系获学士学位</p>
-                <p>1987年于加拿大多伦多大学电子工程系获硕士学位</p>
-                <p>1990年于美国康奈尔大学电子工程学院获博士学位</p>
-                <p>1990年到1998年期间，丁教授在美国奥本大学电子工程系先后担任助理教授和副教授</p>
-                <p>1999年至2000年8月期间在美国爱荷华大学电子与计算机工程系任副教授</p>
-                <p>2000年9月起加入加州大学Davis分校任教授</p>
-                <p>曾在澳大利亚国立大学信息技术与工程系，香港科技大学电子电气工程系，NASA路易斯研究中心，美国佛罗里达空军武器研究实验室担任访问学者或客座教授。</p>
+                <h4><code>刘教授</code><p>博士研究生导师</p></h4>
+                <p>1991年7月本科毕业于曲阜师范大学外语系英语专业</p>
+                <p>1998年至2001年6月在职攻读文艺学专业硕士学位，主要研究西方文论</p>
+                <p>2002年至2003年在北京师范大学比较文学与世界文学专业做高访学者</p>
+                <p>2003年至2006年在北京师范大学攻读比较文学与世界文学专业博士学位，主要研究英美文学</p>
+                <p>2007年至2010年在北京大学博士后流动站（外国语言文化与国际交流学院）从事博士后研究</p>
+                <p>多次参与山东省普通高等教育专升本统一考试命题与阅卷工作</p>
             </span>
-            <img src="/images/nypic4.jpg" width="554" height="500" />
+            <img src="/images/teacher-team.png" width="554" height="500" />
         </li>
         <li>
             <span>
-                <h4><code>陈数</code> 教授<p>山东师范大学</p></h4>
-                <p>1982年于南京工学院（现东南大学）无线电工程系获学士学位</p>
-                <p>1987年于加拿大多伦多大学电子工程系获硕士学位</p>
-                <p>1990年于美国康奈尔大学电子工程学院获博士学位</p>
-                <p>1990年到1998年期间，丁教授在美国奥本大学电子工程系先后担任助理教授和副教授</p>
-                <p>1999年至2000年8月期间在美国爱荷华大学电子与计算机工程系任副教授</p>
-                <p>2000年9月起加入加州大学Davis分校任教授</p>
-                <p>曾在澳大利亚国立大学信息技术与工程系，香港科技大学电子电气工程系，NASA路易斯研究中心，美国佛罗里达空军武器研究实验室担任访问学者或客座教授。</p>
+                <h4><code>张副教授</code><p>硕士研究生导师</p></h4>
+                <p>1992年于山东师范大学数学系毕业，获学士学位</p>
+                <p>1995年于南京大学数学系获硕士学位</p>
+                <p>1995年至今山东某重点高校任教，主讲高等数学、线性代数、概率论等课程</p>
+                <p>2011年晋升为副教授职称，同年担任硕士生导师</p>
+                <p>2014年荣获“校级教学名师”称号</p>
+                <p>2016年荣获“山东省教学名师”称号</p>
+                <p>多次参与山东省普通高等教育专升本统一考试命题与阅卷工作</p>
             </span>
-            <img src="/images/nypic4.jpg" width="554" height="500" />
+            <img src="/images/teacher-team.png" width="554" height="500" />
         </li>
         <li>
             <span>
-                <h4><code>陈数</code> 教授<p>山东师范大学</p></h4>
-                <p>1982年于南京工学院（现东南大学）无线电工程系获学士学位</p>
-                <p>1987年于加拿大多伦多大学电子工程系获硕士学位</p>
-                <p>1990年于美国康奈尔大学电子工程学院获博士学位</p>
-                <p>1990年到1998年期间，丁教授在美国奥本大学电子工程系先后担任助理教授和副教授</p>
-                <p>1999年至2000年8月期间在美国爱荷华大学电子与计算机工程系任副教授</p>
-                <p>2000年9月起加入加州大学Davis分校任教授</p>
-                <p>曾在澳大利亚国立大学信息技术与工程系，香港科技大学电子电气工程系，NASA路易斯研究中心，美国佛罗里达空军武器研究实验室担任访问学者或客座教授。</p>
+                <h4><code>艾教授</code><p>硕士研究生导师</p></h4>
+                <p>1997年9月考入山东大学中国现当代文学专业硕士研究生</p>
+                <p>2000年7月获文学硕士学位</p>
+                <p>2001年9月考入郑州大学中国现当代文学专业博士研究生</p>
+                <p>2004年7月获文学博士学位</p>
+                <p>2008年7月至今任文学院硕士研究生导师</p>
+                <p>2011年5月，在浙江大学中国语言文学博士后流动站从事博士后研究</p>
+                <p>山东省“新世纪十百千人才工程”第二层次人选</p>
+                <p>山东省首批文化名家暨“四个一批”人才</p>
+                <p>多次参与山东省普通高等教育专升本统一考试命题与阅卷工作</p>
             </span>
-            <img src="/images/nypic4.jpg" width="554" height="500" />
+            <img src="/images/teacher-team.png" width="554" height="500" />
         </li>
         <li>
             <span>
-                <h4><code>陈数</code> 教授<p>山东师范大学</p></h4>
-                <p>1982年于南京工学院（现东南大学）无线电工程系获学士学位</p>
-                <p>1987年于加拿大多伦多大学电子工程系获硕士学位</p>
-                <p>1990年于美国康奈尔大学电子工程学院获博士学位</p>
-                <p>1990年到1998年期间，丁教授在美国奥本大学电子工程系先后担任助理教授和副教授</p>
-                <p>1999年至2000年8月期间在美国爱荷华大学电子与计算机工程系任副教授</p>
-                <p>2000年9月起加入加州大学Davis分校任教授</p>
-                <p>曾在澳大利亚国立大学信息技术与工程系，香港科技大学电子电气工程系，NASA路易斯研究中心，美国佛罗里达空军武器研究实验室担任访问学者或客座教授。</p>
+                <h4><code>李副教授</code><p>硕士研究生导师</p></h4>
+                <p>1994年考入北京师范大学信息工程学院计算机科学系</p>
+                <p>1998年获得工学学士学位</p>
+                <p>1998年考入北京交通大学计算机与通信工程学院</p>
+                <p>2001年获得工学硕士学位</p>
+                <p>2001年至今山东某重点高校任教，主讲计算机文化基础、数据结构、数据库等课程</p>
+                <p>2012年至今任计算机学院硕士研究生导师</p>
+                <p>多次参与山东省普通高等教育专升本统一考试命题与阅卷工作</p>
             </span>
-            <img src="/images/nypic4.jpg" width="554" height="500" />
+            <img src="/images/teacher-team.png" width="554" height="500" />
         </li>
         <li>
             <span>
-                <h4><code>陈数</code> 教授<p>山东师范大学</p></h4>
-                <p>1982年于南京工学院（现东南大学）无线电工程系获学士学位</p>
-                <p>1987年于加拿大多伦多大学电子工程系获硕士学位</p>
-                <p>1990年于美国康奈尔大学电子工程学院获博士学位</p>
-                <p>1990年到1998年期间，丁教授在美国奥本大学电子工程系先后担任助理教授和副教授</p>
-                <p>1999年至2000年8月期间在美国爱荷华大学电子与计算机工程系任副教授</p>
-                <p>2000年9月起加入加州大学Davis分校任教授</p>
-                <p>曾在澳大利亚国立大学信息技术与工程系，香港科技大学电子电气工程系，NASA路易斯研究中心，美国佛罗里达空军武器研究实验室担任访问学者或客座教授。</p>
+                <h4><code>王副教授</code><p>硕士研究生导师</p></h4>
+                <p>1996年毕业于山东大学化学系，获学士学位</p>
+                <p>2002年毕业于石河子大学化学院获硕士学位</p>
+                <p>2002年至今任教于山东省某重点高校</p>
+                <p>2008年考入山东中医药大学药学院</p>
+                <p>2012年获得博士学位</p>
+                <p>2013年晋升为副教授职称，同年担任硕士生导师</p>
+                <p>2015年荣获“校级教学名师”称号</p>
+                <p>多次参与山东省普通高等教育专升本统一考试命题与阅卷工作</p>
             </span>
-            <img src="/images/nypic4.jpg" width="554" height="500" />
+            <img src="/images/teacher-team.png" width="554" height="500" />
         </li>
         <li>
             <span>
-                <h4><code>陈数</code> 教授<p>山东师范大学</p></h4>
-                <p>1982年于南京工学院（现东南大学）无线电工程系获学士学位</p>
-                <p>1987年于加拿大多伦多大学电子工程系获硕士学位</p>
-                <p>1990年于美国康奈尔大学电子工程学院获博士学位</p>
-                <p>1990年到1998年期间，丁教授在美国奥本大学电子工程系先后担任助理教授和副教授</p>
-                <p>1999年至2000年8月期间在美国爱荷华大学电子与计算机工程系任副教授</p>
-                <p>2000年9月起加入加州大学Davis分校任教授</p>
-                <p>曾在澳大利亚国立大学信息技术与工程系，香港科技大学电子电气工程系，NASA路易斯研究中心，美国佛罗里达空军武器研究实验室担任访问学者或客座教授。</p>
+                <h4><code>何副教授</code><p>硕士研究生导师</p></h4>
+                <p>2000年毕业于山东大学管理学院并获得管理学学士学位</p>
+                <p>2004年获兰州大学企业管理硕士学位</p>
+                <p>2010年获副教授任职资格</p>
+                <p>2012年1月至5月赴美国中田纳西州立大学访学</p>
+                <p>2004年和2008年先后两次获得山东省优秀教学成果厅级奖</p>
+                <p>2007年本人所讲授的《管理学》课程被评为山东省高校精品课程</p>
+                <p>2008年获“大学生最喜爱的教师”</p>
+                <p>2010年获优秀教学质量奖</p>
+                <p>多次参与山东省普通高等教育专升本统一考试命题与阅卷工作</p>
             </span>
-            <img src="/images/nypic4.jpg" width="554" height="500" />
-        </li>
-        <li>
-            <span>
-                <h4><code>陈数</code> 教授<p>山东师范大学</p></h4>
-                <p>1982年于南京工学院（现东南大学）无线电工程系获学士学位</p>
-                <p>1987年于加拿大多伦多大学电子工程系获硕士学位</p>
-                <p>1990年于美国康奈尔大学电子工程学院获博士学位</p>
-                <p>1990年到1998年期间，丁教授在美国奥本大学电子工程系先后担任助理教授和副教授</p>
-                <p>1999年至2000年8月期间在美国爱荷华大学电子与计算机工程系任副教授</p>
-                <p>2000年9月起加入加州大学Davis分校任教授</p>
-                <p>曾在澳大利亚国立大学信息技术与工程系，香港科技大学电子电气工程系，NASA路易斯研究中心，美国佛罗里达空军武器研究实验室担任访问学者或客座教授。</p>
-            </span>
-            <img src="/images/nypic4.jpg" width="554" height="500" />
+            <img src="/images/teacher-team.png" width="554" height="500" />
         </li>
         </ul>
     </div>
@@ -116,13 +107,12 @@ $this->title = '教研队伍';
         <cite class="gdleft" id="small_left"></cite>
         <div class="scroll_pic" id="smallpic">
             <ul id="small_list">
-            <li class="current"><img src="/images/nypic5.jpg" width="176" height="108" /></li>
-            <li><img src="/images/nypic5.jpg" width="176" height="108" /></li>
-            <li><img src="/images/nypic5.jpg" width="176" height="108" /></li>
-            <li><img src="/images/nypic5.jpg" width="176" height="108" /></li>
-            <li><img src="/images/nypic5.jpg" width="176" height="108" /></li>
-            <li><img src="/images/nypic5.jpg" width="176" height="108" /></li>
-            <li><img src="/images/nypic5.jpg" width="176" height="108" /></li>
+            <li class="current"><img src="/images/teacher-01.png" width="176" height="108" /></li>
+            <li><img src="/images/teacher-02.png" width="176" height="108" /></li>
+            <li><img src="/images/teacher-03.png" width="176" height="108" /></li>
+            <li><img src="/images/teacher-04.png" width="176" height="108" /></li>
+            <li><img src="/images/teacher-05.png" width="176" height="108" /></li>
+            <li><img src="/images/teacher-06.png" width="176" height="108" /></li>
             </ul>
         </div>
         <cite class="gdright" id="small_right"></cite>
@@ -136,46 +126,46 @@ $this->title = '教研队伍';
     <div class="nytxt4_pic">
         <ul>
             <li>
-            <span><a href="#"><img src="/images/nypic6.jpg" width="192" height="257" /></a></span>
-            <span><a href="#"><img src="/images/nypic6.jpg" width="192" height="257" /></a></span>
-            <span><a href="#"><img src="/images/nypic6.jpg" width="192" height="257" /></a></span>
-            <span><a href="#"><img src="/images/nypic6.jpg" width="192" height="257" /></a></span>
-            <span><a href="#"><img src="/images/nypic6.jpg" width="192" height="257" /></a></span>
-            <span><a href="#"><img src="/images/nypic6.jpg" width="192" height="257" /></a></span>
-            <span><a href="#"><img src="/images/nypic6.jpg" width="192" height="257" /></a></span>
-            <span><a href="#"><img src="/images/nypic6.jpg" width="192" height="257" /></a></span>
-            <span><a href="#"><img src="/images/nypic6.jpg" width="192" height="257" /></a></span>
-            <span><a href="#"><img src="/images/nypic6.jpg" width="192" height="257" /></a></span>
-            <span><a href="#"><img src="/images/nypic6.jpg" width="192" height="257" /></a></span>
-            <span><a href="#"><img src="/images/nypic6.jpg" width="192" height="257" /></a></span>
+            <span><a href="#"><img src="/images/tutor/tutor1.png" width="192" height="257" /></a></span>
+            <span><a href="#"><img src="/images/tutor/tutor2.png" width="192" height="257" /></a></span>
+            <span><a href="#"><img src="/images/tutor/tutor3.png" width="192" height="257" /></a></span>
+            <span><a href="#"><img src="/images/tutor/tutor4.png" width="192" height="257" /></a></span>
+            <span><a href="#"><img src="/images/tutor/tutor5.png" width="192" height="257" /></a></span>
+            <span><a href="#"><img src="/images/tutor/tutor6.png" width="192" height="257" /></a></span>
+            <span><a href="#"><img src="/images/tutor/tutor7.png" width="192" height="257" /></a></span>
+            <span><a href="#"><img src="/images/tutor/tutor8.png" width="192" height="257" /></a></span>
+            <span><a href="#"><img src="/images/tutor/tutor9.png" width="192" height="257" /></a></span>
+            <span><a href="#"><img src="/images/tutor/tutor10.png" width="192" height="257" /></a></span>
+            <span><a href="#"><img src="/images/tutor/tutor11.png" width="192" height="257" /></a></span>
+            <span><a href="#"><img src="/images/tutor/tutor12.png" width="192" height="257" /></a></span>
             </li>
             <li>
-            <span><a href="#"><img src="/images/nypic6.jpg" width="192" height="257" /></a></span>
-            <span><a href="#"><img src="/images/nypic6.jpg" width="192" height="257" /></a></span>
-            <span><a href="#"><img src="/images/nypic6.jpg" width="192" height="257" /></a></span>
-            <span><a href="#"><img src="/images/nypic6.jpg" width="192" height="257" /></a></span>
-            <span><a href="#"><img src="/images/nypic6.jpg" width="192" height="257" /></a></span>
-            <span><a href="#"><img src="/images/nypic6.jpg" width="192" height="257" /></a></span>
-            <span><a href="#"><img src="/images/nypic6.jpg" width="192" height="257" /></a></span>
-            <span><a href="#"><img src="/images/nypic6.jpg" width="192" height="257" /></a></span>
-            <span><a href="#"><img src="/images/nypic6.jpg" width="192" height="257" /></a></span>
-            <span><a href="#"><img src="/images/nypic6.jpg" width="192" height="257" /></a></span>
-            <span><a href="#"><img src="/images/nypic6.jpg" width="192" height="257" /></a></span>
-            <span><a href="#"><img src="/images/nypic6.jpg" width="192" height="257" /></a></span>
+            <span><a href="#"><img src="/images/tutor/tutor13.png" width="192" height="257" /></a></span>
+            <span><a href="#"><img src="/images/tutor/tutor14.png" width="192" height="257" /></a></span>
+            <span><a href="#"><img src="/images/tutor/tutor15.png" width="192" height="257" /></a></span>
+            <span><a href="#"><img src="/images/tutor/tutor17.png" width="192" height="257" /></a></span>
+            <span><a href="#"><img src="/images/tutor/tutor19.png" width="192" height="257" /></a></span>
+            <span><a href="#"><img src="/images/tutor/tutor20.png" width="192" height="257" /></a></span>
+            <span><a href="#"><img src="/images/tutor/tutor21.png" width="192" height="257" /></a></span>
+            <span><a href="#"><img src="/images/tutor/tutor22.png" width="192" height="257" /></a></span>
+            <span><a href="#"><img src="/images/tutor/tutor23.png" width="192" height="257" /></a></span>
+            <span><a href="#"><img src="/images/tutor/tutor24.png" width="192" height="257" /></a></span>
+            <span><a href="#"><img src="/images/tutor/tutor25.png" width="192" height="257" /></a></span>
+            <span><a href="#"><img src="/images/tutor/tutor26.png" width="192" height="257" /></a></span>
             </li>
             <li>
-            <span><a href="#"><img src="/images/nypic6.jpg" width="192" height="257" /></a></span>
-            <span><a href="#"><img src="/images/nypic6.jpg" width="192" height="257" /></a></span>
-            <span><a href="#"><img src="/images/nypic6.jpg" width="192" height="257" /></a></span>
-            <span><a href="#"><img src="/images/nypic6.jpg" width="192" height="257" /></a></span>
-            <span><a href="#"><img src="/images/nypic6.jpg" width="192" height="257" /></a></span>
-            <span><a href="#"><img src="/images/nypic6.jpg" width="192" height="257" /></a></span>
-            <span><a href="#"><img src="/images/nypic6.jpg" width="192" height="257" /></a></span>
-            <span><a href="#"><img src="/images/nypic6.jpg" width="192" height="257" /></a></span>
-            <span><a href="#"><img src="/images/nypic6.jpg" width="192" height="257" /></a></span>
-            <span><a href="#"><img src="/images/nypic6.jpg" width="192" height="257" /></a></span>
-            <span><a href="#"><img src="/images/nypic6.jpg" width="192" height="257" /></a></span>
-            <span><a href="#"><img src="/images/nypic6.jpg" width="192" height="257" /></a></span>
+            <span><a href="#"><img src="/images/tutor/tutor27.png" width="192" height="257" /></a></span>
+            <span><a href="#"><img src="/images/tutor/tutor28.png" width="192" height="257" /></a></span>
+            <span><a href="#"><img src="/images/tutor/tutor29.png" width="192" height="257" /></a></span>
+            <span><a href="#"><img src="/images/tutor/tutor30.png" width="192" height="257" /></a></span>
+            <span><a href="#"><img src="/images/tutor/tutor31.png" width="192" height="257" /></a></span>
+            <span><a href="#"><img src="/images/tutor/tutor32.png" width="192" height="257" /></a></span>
+            <span><a href="#"><img src="/images/tutor/tutor33.png" width="192" height="257" /></a></span>
+            <span><a href="#"><img src="/images/tutor/tutor34.png" width="192" height="257" /></a></span>
+            <span><a href="#"><img src="/images/tutor/tutor35.png" width="192" height="257" /></a></span>
+            <span><a href="#"><img src="/images/tutor/tutor36.jpg" width="192" height="257" /></a></span>
+            <span><a href="#"><img src="/images/tutor/tutor18.png" width="192" height="257" /></a></span>
+            <span><a href="#"><img src="/images/tutor/tutor16.png" width="192" height="257" /></a></span>
             </li>
         </ul>
     </div>
@@ -192,3 +182,5 @@ $this->title = '教研队伍';
         <code class="colorfff"><a href="#">申请试听</a></code>
     </dd>
 </dl>
+
+<script src="/js/teacher-intro.js"></script>
