5aee7e4ff07d2be66aedbd897a894b1a23b742d9
minyi <minyi@mobvoi.com>
2018-07-01 11:36:25 +0800

    about

--- frontend/controllers/AboutController.php
+++ frontend/controllers/AboutController.php
@@ -11,10 +11,19 @@ class AboutController extends Controller
     /**
      * @inheritdoc
      */
-    
-    public function actionHowToStudy()
-    {
+
+    // 渲染如何学习静态页
+    public function actionHowToStudy() {
         return $this->render('how-to-study');
     }
 
+    // 渲染关于我们静态页
+    public function actionIndex() {
+        return $this->render('index');
+    }
+
+    // 加入我们静态页渲染
+    public function actionJoin() {
+        return $this->render('join');
+    }
 }
