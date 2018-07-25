23352b9aaeebc2fd899430a7f64761475dd6ba36
wfzhang <wfzhang@mobvoi.com>
2018-07-25 08:22:43 +0800

    update mycourse

---
+++ frontend/views/user/course-copy.php
@@ -0,0 +1,45 @@
+<?php
+
+/* @var $this yii\web\View */
+use yii\helpers\Url;
+use frontend\assets\AppAsset;
+use backend\models\User;
+
+AppAsset::addCss($this,'@web/css/user.css');
+
+$this->title = '个人中心';
+?>
+<div class="container-course menu-position">
+    <div class="container-inner">
+        <span>您的位置：</span>
+        <span><a href="/">首页</a></span>
+        <span>&gt;</span>
+        <span><a href="<?= Url::to(['user/info']) ?>">个人中心</a></span>
+    </div>
+</div>
+<div class="user-wrapper">
+    <?= $this->render('lmenu') ?>
+    <div class="right-content">
+        <p class="user-right-title">我的课程</p>
+        <ul class="user-course-list">
+            <?php foreach ($clist as $key => $course) { ?>
+                <li>
+                    <a href="<?= Url::to(['course/detail', 'courseid' => $course->id]) ?>" class="user-course-img"><img src="<?= $course->list_pic ?>"/></a>
+                    <div class="user-course-details">
+                        <h3><a href="<?= Url::to(['course/detail', 'courseid' => $course->id]) ?>" title="<?= $course->course_name ?>" target="_blank"><?= $course->course_name ?></a></h3>
+                        <div class="row">主讲老师: <?= User::item($course->teacher_id); ?></div>
+                        <div class="row">截止日期: <?= date('Y-m-d', $course_invalid_time[$course->id]) ?></div>
+                        <div class="row">
+                            <div class="btns">
+                                <a class="btn btn-primary" target="_blank" href="<?= Url::to(['course/detail', 'courseid' => $course->id]) ?>">进入学习</a>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            <?php } ?>
+        </ul>
+        <?php if (count($clist) == 0) { ?>
+        <div class="empty-content">您还没有选择任何课程哦~赶紧去<a href="/course/list" class="go-link">挑选课程&gt;&gt;</a></div>
+        <?php } ?>
+    </div>
+</div>
--- frontend/views/user/course.php
+++ frontend/views/user/course.php
@@ -1,45 +1,69 @@
 <?php
-
 /* @var $this yii\web\View */
 use yii\helpers\Url;
 use frontend\assets\AppAsset;
-use backend\models\User;
+use backend\models\Provinces;
+use backend\models\Cities;
 
-AppAsset::addCss($this,'@web/css/user.css');
-
-$this->title = '个人中心';
+$this->title = '我的班级';
 ?>
-<div class="container-course menu-position">
-    <div class="container-inner">
-        <span>您的位置：</span>
-        <span><a href="/">首页</a></span>
-        <span>&gt;</span>
-        <span><a href="<?= Url::to(['user/info']) ?>">个人中心</a></span>
-    </div>
-</div>
-<div class="user-wrapper">
-    <?= $this->render('lmenu') ?>
-    <div class="right-content">
-        <p class="user-right-title">我的课程</p>
-        <ul class="user-course-list">
-            <?php foreach ($clist as $key => $course) { ?>
-                <li>
-                    <a href="<?= Url::to(['course/detail', 'courseid' => $course->id]) ?>" class="user-course-img"><img src="<?= $course->list_pic ?>"/></a>
-                    <div class="user-course-details">
-                        <h3><a href="<?= Url::to(['course/detail', 'courseid' => $course->id]) ?>" title="<?= $course->course_name ?>" target="_blank"><?= $course->course_name ?></a></h3>
-                        <div class="row">主讲老师: <?= User::item($course->teacher_id); ?></div>
-                        <div class="row">截止日期: <?= date('Y-m-d', $course_invalid_time[$course->id]) ?></div>
-                        <div class="row">
-                            <div class="btns">
-                                <a class="btn btn-primary" target="_blank" href="<?= Url::to(['course/detail', 'courseid' => $course->id]) ?>">进入学习</a>
-                            </div>
+<div class="htcontent">
+    <h2 class="htwx cc"><a href="#">首页</a>&gt;<a href="#">我的班级</a></h2>
+    <div class="htbox2">
+        <div class="httxt1 cc">
+            <h3 class="ht_tt1">我的班级</h3>
+            <dl class="cc">
+                <dd class="htqhnow">正在学</dd>
+                <dd class="htqh2">已结课</dd>
+            </dl>
+            <ul>
+                <?php foreach ($clist as $key => $course) { ?>
+                    <li>
+                        <img src="/images/htpic2.jpg" width="298" height="108" />
+                        <h4><?= $course->course_name ?></h4>
+                        <h5><img src="/images/htpic3.jpg" width="70" height="70" /><p>班主任</p></h5>
+                        <div class="tyny">
+                            <p><img src="/images/nyicon1.png" />课堂学（13）</p>
+                            <p><img src="/images/nyicon1a.png" />随堂练（13）</p>
+                            <p><img src="/images/nyicon1b.png" />问老师（18）</p>
+                            <p><img src="/images/nyicon1.png" />模拟考（1）</p>
+                            <p class="httx"><img src="/images/hticon10.png" />同学20</p>
                         </div>
+                    </li>
+                <?php } ?>
+            </ul>
+            <ul style="display:none">
+                <li>
+                    <img src="/images/htpic2.jpg" width="298" height="108" />
+                    <h4>2全能培训班</h4>
+                    <h5><img src="/images/htpic3.jpg" width="70" height="70" /><p>班主任</p></h5>
+                    <div class="tyny">
+                        <p><img src="/images/nyicon1.png" />课堂学（13）</p>
+                        <p><img src="/images/nyicon1a.png" />随堂练（13）</p>
+                        <p><img src="/images/nyicon1b.png" />问老师（18）</p>
+                        <p><img src="/images/nyicon1.png" />模拟考（1）</p>
+                        <p class="httx"><img src="/images/hticon10.png" />同学20</p>
                     </div>
                 </li>
-            <?php } ?>
-        </ul>
-        <?php if (count($clist) == 0) { ?>
-        <div class="empty-content">您还没有选择任何课程哦~赶紧去<a href="/course/list" class="go-link">挑选课程&gt;&gt;</a></div>
-        <?php } ?>
+            </ul>
+        </div>
     </div>
 </div>
+<script type="text/javascript">
+    $(function(){
+        function qiehuan(qhan,qhshow,qhon){
+            $(qhan).click(function(){
+                $(qhan).removeClass(qhon);
+                $(this).addClass(qhon);
+                var i = $(this).index(qhan);
+                $(qhshow).eq(i).show().siblings(qhshow).hide();
+            });
+        }
+        qiehuan(".httxt1 dd",".httxt1 ul","htqhnow");
+
+        $(".httxt2_show dl dt").click(function(){
+            $(".httxt2_show dl").removeClass("ktshow");
+            $(this).parent().addClass("ktshow");
+        })
+    });
+</script>
