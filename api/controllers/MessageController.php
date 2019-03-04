6b919497b7acae6eef33dc50e3ffdbc9e2347527
dev_sky <dev_sky@740051880@qq.com>
2019-03-04 13:58:37 +0800

    市场消息中心

---
+++ api/controllers/MessageController.php
@@ -0,0 +1,23 @@
+<?php
+namespace api\controllers;
+
+use yii;
+use yii\web\Controller;
+use backend\models\Message;
+
+class MessageController extends Controller {
+  //消息中心列表
+  public function actionList(){
+    $messages = Message::find()
+        ->orderBy('publish_time desc')
+        ->all();
+    $message_arr = array();
+        foreach ($messages as $key => $message) {
+            $message_arr[] = array(
+                'title' => $message->title,
+                'publish_time' => $message->publish_time,
+            );
+        }
+        return json_encode($message_arr);
+  }
+}
