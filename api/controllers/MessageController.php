e60c367fe157eec14701c56dfcb2d7d929d655c6
dev_sky <dev_sky@740051880@qq.com>
2019-03-04 15:53:32 +0800

    市场端消息中心更新

--- api/controllers/MessageController.php
+++ api/controllers/MessageController.php
@@ -6,18 +6,22 @@ use yii\web\Controller;
 use backend\models\Message;
 
 class MessageController extends Controller {
-  //消息中心列表
+  //市场专员消息中心列表
   public function actionList(){
-    $messages = Message::find()
-        ->orderBy('publish_time desc')
-        ->all();
-    $message_arr = array();
-        foreach ($messages as $key => $message) {
-            $message_arr[] = array(
-                'title' => $message->title,
-                'publish_time' => $message->publish_time,
+        $get = Yii::$app->request->get();
+        $access_token = $get['access-token'];
+        $user = \common\models\User::findIdentityByAccessToken($access_token);
+        $msgs = Message::find()
+            ->where(['publisher' => $user->id])
+            ->orderBy('publish_time desc')
+            ->all();
+        $msg_arr = array();
+        foreach ($msgs as $key => $msg) {
+            $msg_arr[] = array(
+                'title' => $msg->title,
+                'publish_time' => $msg->publish_time,
             );
         }
-        return json_encode($message_arr);
+        return json_encode($msg_arr);
   }
 }
