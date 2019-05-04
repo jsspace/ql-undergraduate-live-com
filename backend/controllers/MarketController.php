28154d1ea954a7c7b7844e88f2a31b7fc7af556c
bjfudxj <1031848267@qq.com>
2019-05-04 23:08:13 +0800

    pc收入明细统计

--- backend/controllers/MarketController.php
+++ backend/controllers/MarketController.php
@@ -21,6 +21,9 @@ use backend\models\Withdraw;
  */
 class MarketController extends Controller
 {
+    const INCOMEP = 0.2; // 直接收益提成
+    const INCOMENEXTP = 0.2; // 间接收益提成
+
     /**
      * @inheritdoc
      */
@@ -88,12 +91,197 @@ class MarketController extends Controller
         ]);
     }
 
+    // 获取所有学员id @zhang
+    public static function getStudentIds($invite_id) {
+        $students = User::find()
+        ->where(['invite' => $invite_id])
+        ->with(['role' => function($query) {
+            $query->where(['item_name' => 'student']);
+        }])
+        ->all();
+        $student_ids = array();
+        foreach ($students as $student) {
+            if (count($student->role) > 0) {
+                $student_ids[] = $student->id;
+            }
+        }
+        return $student_ids;
+    }
+    // 获取直接下级id @zhang
+    public static function getMarketIds($invite_id) {
+        $market_ids = array();
+        $marketers = User::find()
+        ->where(['invite' => $invite_id])
+        ->with(['role' => function($query) {
+            $query->where(['item_name' => 'marketer']);
+        }])
+        ->all();
+        foreach ($marketers as $key => $marketer) {
+            if (count($marketer->role) > 0) {
+                $market_ids[] = $marketer->id;
+            }
+        }
+        return $market_ids;
+    }
+
     /**
      * Displays a single User model.
      * @param integer $id
      * @return mixed
      */
-    public function actionOrder($userid, $month=null)
+    // 递归计算当前用户的按月统计收益 @zhang
+    public static function getMonthIncome($id) {
+        $market_ids = self::getMarketIds($id);
+        // 直接下级为空
+        if (count($market_ids) == 0) {
+            $student_ids = self::getStudentIds($id);
+            $direct_orders = OrderInfo::find()
+            ->select(["sum(order_amount)*".self::INCOMEP." as income, DATE_FORMAT(FROM_UNIXTIME(pay_time, '%Y-%m-%d'), '%Y-%m') as month"])
+            ->where(['in', 'user_id', $student_ids])
+            ->andWhere(['order_status' => 1])
+            ->andWhere(['pay_status' => 2])
+            ->groupBy(["DATE_FORMAT(FROM_UNIXTIME(pay_time, '%Y-%m-%d'), '%Y-%m') DESC"])
+            ->asArray()
+            ->all();
+            return $direct_orders;
+        } else {
+            $student_ids = self::getStudentIds($id);
+            $direct_orders = OrderInfo::find()
+            ->select(["sum(order_amount)*".self::INCOMEP." as income, DATE_FORMAT(FROM_UNIXTIME(pay_time, '%Y-%m-%d'), '%Y-%m') as month"])
+            ->where(['in', 'user_id', $student_ids])
+            ->andWhere(['order_status' => 1])->andWhere(['pay_status' => 2])
+            ->groupBy(["DATE_FORMAT(FROM_UNIXTIME(pay_time, '%Y-%m-%d'), '%Y-%m') DESC"])
+            ->asArray()
+            ->all();
+            foreach ($market_ids as $key => $market_id) {
+                $sub_income = self::getMonthIncome($market_id); // 获取每个子代理的总收益
+                foreach ($sub_income as &$income) {
+                    $income['income'] = $income['income'] * self::INCOMENEXTP;
+                }
+                unset($income);
+                $direct_orders = array_merge($direct_orders, $sub_income);
+                for ($i = 0; $i < count($direct_orders)-1; $i++) {
+                    for ($j = $i+1; $j < count($direct_orders); $j++) {
+                        if ($direct_orders[$i]['month'] == $direct_orders[$j]['month']) {
+                            $direct_orders[$i]['income'] += $direct_orders[$j]['income'] ;
+                            unset($direct_orders[$j]);
+                            $direct_orders = array_values($direct_orders);
+                            $j = $j-1;
+                        }
+                    }
+                }
+            }
+            return $direct_orders;
+        }
+    }
+
+    // 按月统计收入明细
+    public function actionOrder($userid, $username) {
+        $my_income = self::getMonthIncome($userid);
+        array_multisort(array_column($my_income,'month'),SORT_DESC,$my_income);
+        foreach ($my_income as $key => $item) {
+            // 查询每个月的提成是否已经确认提现
+            $withdraw_info = Withdraw::find()
+            ->where(['user_id' => $userid, 'withdraw_date' => $my_income[$key]['month']])->one();
+            $my_income[$key]['status'] = !empty($withdraw_info) ? '已结算' : '未结算';
+        }
+        return $this->render('order',[
+            'income' => $my_income,
+            'userid' => $userid,
+            'username' => $username
+        ]);
+    }
+
+    // 递归计算用户的某一个月份的统计收益 @zhang
+    public static function getOneMonthIncome($id, $month) {
+        $market_ids = self::getMarketIds($id);
+        // 直接下级为空
+        if (count($market_ids) == 0) {
+            $student_ids = self::getStudentIds($id);
+            $direct_orders = OrderInfo::find()
+            ->select(["sum(order_amount)*".self::INCOMEP." as income"])
+            ->where(['in', 'user_id', $student_ids])
+            ->andWhere(["DATE_FORMAT(FROM_UNIXTIME(pay_time, '%Y-%m-%d'), '%Y-%m')" => $month])
+            ->andWhere(['order_status' => 1])
+            ->andWhere(['pay_status' => 2])
+            ->asArray()
+            ->all();
+            return $direct_orders[0]['income'];
+        } else {
+            $student_ids = self::getStudentIds($id);
+            $direct_orders = OrderInfo::find()
+            ->select(["sum(order_amount)*".self::INCOMEP." as income"])
+            ->where(['in', 'user_id', $student_ids])
+            ->andWhere(["DATE_FORMAT(FROM_UNIXTIME(pay_time, '%Y-%m-%d'), '%Y-%m')" => $month])
+            ->andWhere(['order_status' => 1])
+            ->andWhere(['pay_status' => 2])
+            ->asArray()
+            ->all();
+            $income = $direct_orders[0]['income'];
+            foreach ($market_ids as $key => $market_id) {
+                $sub_income = self::getOneMonthIncome($market_id, $month); // 获取每个子代理的总收益
+                $income += $sub_income * self::INCOMENEXTP;
+            }
+            return $income;
+        }
+    }
+
+    // 具体月份收益明细（直接和间接）@zhang
+    public function actionOrderDetail($userid, $month, $username) {
+        $direct_income = array();
+        // 1、查询直接注册的用户
+        $student_ids = self::getStudentIds($userid);
+        // 2、根据直接注册的用户查询邀请的用户下单情况并计算收益
+        $orders = OrderInfo::find()->select(['consignee', 'order_amount', 'pay_time', 'user_id'])
+            ->where(['in', 'user_id', $student_ids])
+            ->andWhere(["DATE_FORMAT(FROM_UNIXTIME(pay_time, '%Y-%m-%d'), '%Y-%m')" => $month])
+            ->andWhere(['order_status' => 1])
+            ->andWhere(['pay_status' => 2])
+            ->all();
+        foreach ($orders as $order) {
+            $userpic = User::find()->select(['picture'])
+            ->where(['id' => $order->user_id])
+            ->one();
+            $order_content = array(
+                'pic' => $userpic->picture,
+                'consignee' => $order->consignee,
+                'order_amount' => $order->order_amount,
+                'income' => round($order->order_amount * self::INCOMEP, 2),
+                'pay_time' => date('Y-m-d H:i:s', $order->pay_time)
+            );
+            $direct_income[] = $order_content;
+        }
+        array_multisort(array_column($direct_income,'pay_time'),SORT_DESC, $direct_income);
+        
+        // 1、查找自己的直接下级代理
+        $market_ids = self::getMarketIds($userid);
+        $indirect_income = array();
+        // 2、查找每个直接下级代理所邀请的学生的订单
+        foreach ($market_ids as $market_id) {
+            $income = self::getOneMonthIncome($market_id, $month);
+            // 3、将订单信息添加到数组并计算该笔订单的间接收益
+            $user = User::find()
+            ->select(['picture', 'username'])
+            ->where(['id' => $market_id])
+            ->one();
+            if ($income > 0) {
+                $order_content = array(
+                    'pic' => $user->picture,
+                    'username' => $user->username,
+                    'income' => $income,
+                    'tc' => $income * self::INCOMENEXTP
+                );
+                $indirect_income[] = $order_content;
+            }
+        }
+
+        return $this->render('order-detail',[
+            'direct' => $direct_income,
+            'indirect' => $indirect_income,
+            'username' => $username
+        ]);
+    }
+    public function actionOrder1($userid, $month=null)
     {
         $roles_array = Yii::$app->authManager->getRolesByUser(Yii::$app->user->id);
         if (!array_key_exists('admin',$roles_array) && $userid != Yii::$app->user->id) {
--- backend/views/market/order.php
+++ backend/views/market/order.php
@@ -2,34 +2,30 @@
 
 use yii\widgets\LinkPager;
 use backend\models\User;
+use yii\helpers\Url;
+use backend\assets\AppAsset;
 
 /* @var $this yii\web\View */
 /* @var $model backend\models\User */
 
-$this->title = Yii::t('app', '订单记录');
-?>
-<div class="user-view">
-    <div class="order-table order-history">
-        <ul>
-            <li>
-                <label class="tr-title">订单号</label>
-                <label class="tr-title">商品总金额</label>
-                <label class="tr-title">用户ID</label>
-                <label class="tr-title">订单生成时间</label>
-            </li>
-            <?php foreach($model as $key=>$val) { ?>
-                <li>
-                    <span class="tr-title"><a href="/order-info/view?id=<?= $val->order_id ?>" target="_blank"><?= $val->order_sn ?></a></span>
-                    <span class="tr-title"><?= $val->goods_amount ?></span>
-                    <span class="tr-title"><a href="/student/<?= $val->user_id ?>" target="_blank"><?= User::item($val->user_id); ?></a></span>
-                    <span class="tr-title"><?= date('Y-m-d H:i:s', $val->add_time) ?></span>
-                </li>
-            <?php } ?>
-        </ul>
-    </div>
-    <?= LinkPager::widget(['pagination' => $pages]); ?>
+$this->title = Yii::t('app', '按月收益统计表('.$username.')');
 
-	<div>市场专员报酬 = 本区域订单×提成比例 提成比例初定为50%</div>
-    <div>总收入：<?= $market_total_income; ?></div>
-    <div>已提现金额：<?= $total_withdraw; ?></div>
+AppAsset::addCss($this,'@web/css/market.css');
+
+?>
+<div class="user-view market-earnings">
+    <ul class="title">
+        <li>月份</li>
+        <li>提成</li>
+        <li>状态</li>
+        <li>操作</li>
+    </ul>
+    <?php foreach ($income as $item) { ?>
+        <ul>
+            <li><?= $item['month'] ?></li>
+            <li><?= $item['income'] ?></li>
+            <li><?= $item['status'] ?></li>
+            <li><a href="<?= Url::to(['market/order-detail', 'userid' => $userid, 'month' => $item['month'], 'username' => $username]) ?>">明细</a></li>
+        </ul>
+    <?php } ?>
 </div>
